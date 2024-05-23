d2d285b886bdf541573836836a6b79799e822314
Beellal <mahamanehabiboubilal999@gmail.com>
2024-05-23 07:05:57 -0700

    Full creation

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,11 +1,8 @@
 -- Script to create and populate the table second_table
-CREATE TABLE IF NOT EXISTS second_table (
-    id INT,
-    name VARCHAR(256),
-    score INT
-);
+CREATE TABLE IF NOT EXISTS second_table (id INT, name VARCHAR(256), score INT);
 
-INSERT INTO hbtn_0c_0.second_table (id, name, score) VALUES (1, 'John', 10);
-INSERT INTO hbtn_0c_0.second_table (id, name, score) VALUES (2, 'Alex', 3);
-INSERT INTO hbtn_0c_0.second_table (id, name, score) VALUES (3, 'Bob', 14);
-INSERT INTO hbtn_0c_0.second_table (id, name, score) VALUES (4, 'George', 8);
+-- Script should create maximum of 4 records
+INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (1, "John", 10);
+INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (2, "Alex", 3);
+INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (3, "Bob", 14);
+INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (4, "George", 8);
