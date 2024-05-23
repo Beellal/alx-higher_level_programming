58325b3c533e66f384914739c866baee3418ab46
Beellal <mahamanehabiboubilal999@gmail.com>
2024-05-23 06:47:35 -0700

    Full Des

--- 0x0D-SQL_introduction/5-full_table.sql
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -1,4 +1,2 @@
 -- Script to print the full description of the table first_table
-SELECT column_name, column_type, IS_NULLABLE, column_default
-FROM information_schema.columns
-WHERE table_schema = 'hbtn_0c_0' AND table_name = 'first_table';
+SHOW CREATE TABLE first_table;
